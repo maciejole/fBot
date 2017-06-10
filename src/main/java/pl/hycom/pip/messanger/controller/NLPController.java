0c6dc4c895fdc539d942a0652d966fb5f319d144
Piotr Bednarek <bednarek.piotr@vp.pl>
2017-06-10 22:49:21 +0200

    usunięcie wstrzyknięcia

--- src/main/java/pl/hycom/pip/messanger/controller/NLPController.java
+++ src/main/java/pl/hycom/pip/messanger/controller/NLPController.java
@@ -20,9 +20,9 @@ import java.util.List;
 @Log4j2
 @Controller
 public class NLPController {
-
-    @Autowired
-    private NlpService nlpService;
+//
+//    @Autowired
+//    private NlpService nlpService;
 
     public static List<Result> outputList = new ArrayList<>();
 
@@ -32,9 +32,9 @@ public class NLPController {
     @RequestMapping(value = "/admin/nlp", method = RequestMethod.GET)
     public String returnView(Model model) {
         List<Result> temp = outputList;
-        if (!outputList.isEmpty()) {
-            temp = nlpService.matchKeywords(outputList);
-        }
+//        if (!outputList.isEmpty()) {
+//            temp = nlpService.matchKeywords(outputList);
+//        }
         model.addAttribute("lista",temp);
         return NLP_VIEW;
     }
